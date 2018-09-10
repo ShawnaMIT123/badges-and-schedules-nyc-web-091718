335b83eb255d9edf79f1a7669965b80faa41582e
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-10 18:28:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,14 @@
 def badge_maker(name)
     "Hello, my name is #{name}."
 end
+
 def batch_badge_creator(array)
   array.collect do |name|
     "Hello, my name is #{name}."
   end
+end
+
+def assign_rooms(array)
+   array.each_with_index do |name, idx|
+    "Hello, my name is #{name}."
 end
